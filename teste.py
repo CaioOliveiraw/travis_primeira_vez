4651381dc86826815eb3c6f803d4f8ed1c33a2ed
Caio Oliviera <caio.ribeiro@aluno.faculdadeimpacta.com.br>
2019-03-18 22:16:22 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import pytest
-import somar from principal 
-import subtrair from principal 
+import somar from principal
+import subtrair from principal
 def test_somar():
     assert somar(2,4)==6
